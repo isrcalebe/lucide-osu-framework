9dd6267c62dff436e0b23b9cc3ca29ff99390815
isrcalebe <work@isrcalebe.cc>
2025-01-31 08:46:53 -0300

    feat: new tests

--- source/tests/lucide/Visual/TestSceneLucideIcons.cs
+++ source/tests/lucide/Visual/TestSceneLucideIcons.cs
@@ -1,130 +1,189 @@
 using System;
+using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
+using System.Reflection;
+using NUnit.Framework;
+using osu.Framework.Extensions.IEnumerableExtensions;
 using osu.Framework.Graphics.Containers;
+using osu.Framework.Graphics.Cursor;
+using osu.Framework.Graphics.Shapes;
 using osu.Framework.Graphics.Sprites;
-using osu.Framework.Graphics.UserInterface;
+using osu.Framework.Localisation;
+using osu.Framework.Threading;
 
 namespace Lucide.OsuFramework.Tests.Visual;
 
 public partial class TestSceneLucideIcons : LucideTestScene
 {
-    private SpriteIcon? spriteIcon;
-    private FillFlowContainer? info;
+    private ScheduledDelegate? scheduledDelegate;
 
-    private SpriteText? iconClassNameText, iconUnicodeText, iconSizeText;
-
-    private BasicButton? previousButton, nextButton;
-
-    private int currentIndex = 0;
-
-    private BasicHexColourPicker? colourPicker;
-
-    [BackgroundDependencyLoader]
-    private void load()
+    [Test]
+    public void TestPreloadIcon()
     {
-        AddRange(
-        [
-            info = new FillFlowContainer
+        SpriteIcon? icon = null;
+
+        AddStep("create icon", () => icon = new SpriteIcon
+        {
+            Size = new Vector2(20),
+            Icon = LucideIcons.All.Anchor
+        });
+        AddStep("preload icon", () => LoadComponent(icon));
+        AddStep("change icon", () => icon!.Icon = LucideIcons.All.Archive);
+        AddStep("add icon", () => Add(icon));
+    }
+
+    [Test]
+    public void TestOneIconAtTime()
+    {
+        FillFlowContainer flow = null!;
+        Icon[] icons = null!;
+
+        var i = 0;
+
+        AddStep("prepare test", () =>
+        {
+            i = 0;
+            icons = [.. getAllIcons()];
+            scheduledDelegate?.Cancel();
+
+            Child = new TooltipContainer
             {
-                Anchor = Anchor.TopLeft,
-                Origin = Anchor.TopLeft,
-                Direction = FillDirection.Vertical,
-                AutoSizeAxes = Axes.Both,
+                RelativeSizeAxes = Axes.Both,
                 Children =
                 [
-                    iconClassNameText = new SpriteText(),
-                    iconUnicodeText = new SpriteText(),
-                    iconSizeText = new SpriteText(),
+                    new BasicScrollContainer
+                    {
+                        RelativeSizeAxes = Axes.Both,
+                        Child = flow = new FillFlowContainer
+                        {
+                            Anchor = Anchor.TopRight,
+                            Origin = Anchor.TopRight,
+                            RelativeSizeAxes = Axes.X,
+                            AutoSizeAxes = Axes.Y,
+                            Spacing = new Vector2(5),
+                            Direction = FillDirection.Full
+                        }
+                    }
                 ]
-            },
-            colourPicker = new BasicHexColourPicker
-            {
-                Anchor = Anchor.BottomLeft,
-                Origin = Anchor.BottomLeft,
-            },
-            previousButton = new BasicButton
-            {
-                Anchor = Anchor.CentreLeft,
-                Origin = Anchor.CentreLeft,
-                Margin = new MarginPadding { Left = 10 },
-                Text = "Previous",
-                Action = previousIcon,
-                Size = new Vector2(100, 50)
-            },
-            nextButton = new BasicButton
-            {
-                Anchor = Anchor.CentreRight,
-                Origin = Anchor.CentreRight,
-                Margin = new MarginPadding { Right = 10 },
-                Text = "Next",
-                Action = nextIcon,
-                Size = new Vector2(100, 50)
-            },
-            spriteIcon = new SpriteIcon
-            {
-                Anchor = Anchor.Centre,
-                Origin = Anchor.Centre,
-                Size = new Vector2(24),
-            }
-        ]);
+            };
+        });
 
-        var icon = LucideIcons.IconsUnicode.Values.ElementAt(currentIndex);
-        var name = LucideIcons.IconsUnicode.Keys.ElementAt(currentIndex);
-
-        iconClassNameText!.Text = $"Icon Class Name: {name}";
-        iconUnicodeText!.Text = $"Icon Unicode: 0x{icon:X}";
-
-        spriteIcon!.Icon = LucideIcons.Get(icon);
-
-        iconSizeText!.Text = $"Icon Size: {spriteIcon.Size}";
-
-        colourPicker.Current.ValueChanged += e => spriteIcon.Colour = e.NewValue;
-
-        AddSliderStep("Icon Size", 16, 48, 24, size =>
+        AddStep("start adding icons", () =>
         {
-            spriteIcon.Size = new Vector2(size);
-            iconSizeText.Text = $"Icon Size: {size}";
+            scheduledDelegate = Scheduler.AddDelayed(() =>
+            {
+                flow.Add(icons[i++]);
+
+                if (++i > icons.Length - 1)
+                    scheduledDelegate?.Cancel();
+            }, 5, true);
         });
     }
 
-    private void nextIcon()
+    [Test]
+    public void TestLoadAllIcons()
     {
-        if (currentIndex < LucideIcons.IconsUnicode.Count - 1)
-            currentIndex++;
-        else
-            currentIndex = 0;
+        Box background = null!;
+        FillFlowContainer flow = null!;
 
-        var icon = LucideIcons.IconsUnicode.Values.ElementAt(currentIndex);
-        var name = LucideIcons.IconsUnicode.Keys.ElementAt(currentIndex);
+        AddStep("prepare test", () =>
+        {
+            scheduledDelegate?.Cancel();
+            Child = new TooltipContainer
+            {
+                RelativeSizeAxes = Axes.Both,
+                Children =
+                [
+                    background = new Box
+                    {
+                        Colour = Colour4.Teal,
+                        RelativeSizeAxes = Axes.Both,
+                    },
+                    new BasicScrollContainer
+                    {
+                        RelativeSizeAxes = Axes.Both,
+                        Child = flow = new FillFlowContainer
+                        {
+                            Anchor = Anchor.TopRight,
+                            Origin = Anchor.TopRight,
+                            RelativeSizeAxes = Axes.X,
+                            AutoSizeAxes = Axes.Y,
+                            Spacing = new Vector2(5),
+                            Direction = FillDirection.Full,
+                        },
+                    }
+                ]
+            };
 
-        iconClassNameText!.Text = $"Icon Class Name: {name}";
-        iconUnicodeText!.Text = $"Icon Unicode: 0x{icon:X}";
+            var categories = typeof(LucideIcons).GetNestedTypes();
 
-        spriteIcon!.Icon = LucideIcons.Get(icon);
+            foreach (var category in categories)
+            {
+                flow.Add(new SpriteText
+                {
+                    Text = category.Name,
+                    Scale = new Vector2(4),
+                    RelativeSizeAxes = Axes.X,
+                    Padding = new MarginPadding(10),
+                });
 
-        iconSizeText!.Text = $"Icon Size: {spriteIcon.Size}";
+                foreach (var icon in getAllIconsForCategory(category))
+                    flow.Add(icon);
+            }
+        });
+        AddStep("toggle shadows", () => flow.Children.OfType<Icon>().ForEach(i => i.SpriteIcon.Shadow = !i.SpriteIcon.Shadow));
+        AddStep("change icons", () => flow.Children.OfType<Icon>().ForEach(i => i.SpriteIcon.Icon = new IconUsage((char)(i.SpriteIcon.Icon.Icon + 1))));
+        AddStep("white background", () => background.FadeColour(Colour4.White, 200));
+        AddStep("move shadow offset", () => flow.Children.OfType<Icon>().ForEach(i => i.SpriteIcon.ShadowOffset += Vector2.One));
+        AddStep("change shadow colour", () => flow.Children.OfType<Icon>().ForEach(i => i.SpriteIcon.ShadowColour = Colour4.Pink));
+        AddStep("add new icon with colour and offset", () =>
+            flow.Add(new Icon("LucideIcons.All.Handshake", LucideIcons.All.Handshake)
+            {
+                SpriteIcon = { Shadow = true, ShadowColour = Colour4.Orange, ShadowOffset = new Vector2(5, 1) }
+            }));
     }
 
-    private void previousIcon()
+    private static IEnumerable<Icon> getAllIcons()
     {
-        if (currentIndex > 0)
-            currentIndex--;
-        else
-            currentIndex = LucideIcons.IconsUnicode.Count - 1;
+        var categories = typeof(LucideIcons).GetNestedType("All") ?? throw new InvalidOperationException("Could not find All class in LucideIcons.");
 
-        var icon = LucideIcons.IconsUnicode.Values.ElementAt(currentIndex);
-        var name = LucideIcons.IconsUnicode.Keys.ElementAt(currentIndex);
+        foreach (var property in categories.GetProperties(BindingFlags.Public | BindingFlags.Static))
+        {
+            var value = property.GetValue(null);
+            Debug.Assert(value != null);
 
-        iconClassNameText!.Text = $"Icon Class Name: {name}";
-        iconUnicodeText!.Text = $"Icon Unicode: 0x{icon:X}";
-
-        spriteIcon!.Icon = LucideIcons.Get(icon);
-
-        iconSizeText!.Text = $"Icon Size: {spriteIcon.Size}";
+            yield return new Icon($"{nameof(LucideIcons)}.{property.Name}", (IconUsage)value);
+        }
     }
 
-    private void testIcon(IconUsage icon)
+    private static IEnumerable<Icon> getAllIconsForCategory(Type category)
     {
-        spriteIcon!.Icon = icon;
+        foreach (var p in category.GetProperties(BindingFlags.Public | BindingFlags.Static))
+        {
+            object? propValue = p.GetValue(null);
+            Debug.Assert(propValue != null);
+
+            yield return new Icon($"{nameof(LucideIcons)}.{category.Name}.{p.Name}", (IconUsage)propValue);
+        }
+    }
+
+    private partial class Icon : Container, IHasTooltip
+    {
+        public LocalisableString TooltipText { get; }
+
+        public SpriteIcon SpriteIcon { get; }
+
+        public Icon(string name, IconUsage icon)
+        {
+            TooltipText = name;
+            AutoSizeAxes = Axes.Both;
+
+            Child = SpriteIcon = new SpriteIcon
+            {
+                Icon = icon,
+                Size = new Vector2(60),
+            };
+        }
     }
 }
